f5014938d0e57d416c774f2e18247584e4795f6c
Kevin Tu <tukevin274@gmail.com>
2025-02-20 21:04:05 -0700

    Add javadocs

--- code/app/src/main/java/com/example/project_seekdeep/SocialSituations.java
+++ code/app/src/main/java/com/example/project_seekdeep/SocialSituations.java
@@ -2,8 +2,12 @@ package com.example.project_seekdeep;
 
 import androidx.annotation.NonNull;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum SocialSituations {
     ALONE("Alone"),
+    WITH_ANOTHER("With Another Person"),
     SEVERAL_PEOPLE("Two or Several People"),
     CROWD("With a Crowd");
 
@@ -12,9 +16,27 @@ public enum SocialSituations {
         this.formattedName = formattedName;
     }
 
+    /**
+     * Override toString to provide access to the string value of an enum.
+     * @return
+     *      String representation of a given enum.
+     */
     @NonNull
     @Override
     public String toString() {
         return formattedName;
+    }
+
+    /**
+     * Primarily used to port this enum to be used in an adapter of some sort. E.g, ArrayAdapter, Spinner.
+     * @return
+     *      A list of string values corresponding to each Social Situation enum.
+     */
+    public List<String> getStringValues() {
+        List<String> ret = new ArrayList<>();
+        for (SocialSituations s : SocialSituations.values()) {
+            ret.add(s.formattedName);
+        }
+        return ret;
     }
 }
