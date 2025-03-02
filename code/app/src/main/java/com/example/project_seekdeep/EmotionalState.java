a0691856ec3130709749ba4af7e3a2d56dbbf5fe
Jshell0 <jachelle@ualberta.ca>
2025-03-02 13:37:47 -0700

    Added comments (javadocs)

--- code/app/src/main/java/com/example/project_seekdeep/EmotionalState.java
+++ code/app/src/main/java/com/example/project_seekdeep/EmotionalState.java
@@ -1,7 +1,11 @@
 package com.example.project_seekdeep;
 
+/**
+ * This is a class that represents the emotional state of the user.
+ */
 public enum EmotionalState {
     // these representations and colours can be changed later
+    // the colours may change
     ANGER("\uD83D\uDE20", "#CD5C5C"),  // RED
     CONFUSION("\uD83E\uDD14", "#e59866"),  // ORANGE
     DISGUST("\uD83E\uDD22", "#52be80 "),  // GREEN
@@ -17,18 +21,38 @@ public enum EmotionalState {
         this.colour = colour;
     }
     // getters and setters
+
+    /**
+     * This method fetches the emoticon associated with the mood
+     * @return
+     *      Returns the emoticon that represents the mood
+     */
     public String getEmoticon() {
         return emoticon;
     }
 
+    // setters might be removed later! //
+    /**
+     * This method sets the emoticon for the mood
+     * @param emoticon
+     */
     public void setEmoticon(String emoticon) {
         this.emoticon = emoticon;
     }
 
+    /**
+     * This method fetches the colour for the mood
+     * @return
+     *      Returns the colour of the mood
+     */
     public String getColour() {
         return colour;
     }
 
+    /**
+     * This method sets the colour for the mood
+     * @param colour
+     */
     public void setColour(String colour) {
         this.colour = colour;
     }
