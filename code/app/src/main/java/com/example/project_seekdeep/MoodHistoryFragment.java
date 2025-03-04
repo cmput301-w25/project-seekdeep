b3aff16f0de78833bceff04fd310d9b3a2aabc11
Kevin Tu <tukevin274@gmail.com>
2025-03-03 20:56:47 -0700

    Add base functionality to navigation bar

--- code/app/src/main/java/com/example/project_seekdeep/MoodHistoryFragment.java
+++ code/app/src/main/java/com/example/project_seekdeep/MoodHistoryFragment.java
@@ -3,6 +3,13 @@ package com.example.project_seekdeep;
 
 import androidx.fragment.app.Fragment;
 
-public class MoodHistoryFragment extends Fragment {
+/**
+ * This fragment class is designed to display a list of posted moods by a given user.
+ * @author Kevin Tu
+ */
 
+public class MoodHistoryFragment extends Fragment {
+    public MoodHistoryFragment() {
+        super(R.layout.fragment_mood_history);
+    }
 }
