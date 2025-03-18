8ab8d619c2118967a04b19f13c22735e3ddf5a57
Kevin Tu <tukevin274@gmail.com>
2025-03-18 16:46:31 -0600

    Add class to show mood's full details

--- code/app/src/main/java/com/example/project_seekdeep/FeedFragment.java
+++ code/app/src/main/java/com/example/project_seekdeep/FeedFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -142,6 +143,10 @@ public class FeedFragment extends Fragment {
                 }
                 moodArrayAdapter.notifyDataSetChanged();
             }
+        });
+        // From lab 3,
+        moodListView.setOnClickListener(v -> {
+
         });
     }
 
---
+++ code/app/src/main/java/com/example/project_seekdeep/MoodFullDetailsFragment.java
@@ -0,0 +1,11 @@
+package com.example.project_seekdeep;
+
+/**
+ * This fragment class is used to show the full details of a mood that the user has tapped on
+ * in a ListView. This fragment will display the Mood's original card as seen on the list view,
+ * the original details of who posted, when, etc, and the comments associated with the mood.
+ * @author Kevin Tu
+ */
+public class MoodFullDetailsFragment {
+    
+}
