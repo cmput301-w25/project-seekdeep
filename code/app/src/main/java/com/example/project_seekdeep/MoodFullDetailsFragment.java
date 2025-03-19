f1ced92be811274744fbe79635debfb0a278d266
Kevin Tu <tukevin274@gmail.com>
2025-03-18 19:37:44 -0600

    Finish fragment layout for mood details and comments

--- code/app/src/main/java/com/example/project_seekdeep/MoodFullDetailsFragment.java
+++ code/app/src/main/java/com/example/project_seekdeep/MoodFullDetailsFragment.java
@@ -1,11 +1,50 @@
 package com.example.project_seekdeep;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 /**
  * This fragment class is used to show the full details of a mood that the user has tapped on
  * in a ListView. This fragment will display the Mood's original card as seen on the list view,
  * the original details of who posted, when, etc, and the comments associated with the mood.
  * @author Kevin Tu
  */
-public class MoodFullDetailsFragment {
+public class MoodFullDetailsFragment extends Fragment {
+    private ImageButton backButton;
+    private TextView headerText;
+    private RecyclerView commentView;
+    private ArrayList<Comment> comments;
+    private Mood clickedOnMood;
+    private MoodArrayAdapter moodArrayAdapter;
 
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_mood_details_and_comments, container, false);
+        backButton = view.findViewById(R.id.back_button);
+        headerText = view.findViewById(R.id.whose_mood_text);
+        commentView = view.findViewById(R.id.comments_recycler_view);
+
+        // Get passed data from previous fragment
+        assert savedInstanceState != null;
+        Bundle commentsBundle =  savedInstanceState.getBundle("comments");
+        Bundle clickedOnMoodBundle = savedInstanceState.getBundle("mood");
+        comments = (ArrayList<Comment>) commentsBundle.get("comments");
+        clickedOnMood = (Mood) clickedOnMoodBundle.get("mood");
+
+
+
+        return view;
+    }
 }
