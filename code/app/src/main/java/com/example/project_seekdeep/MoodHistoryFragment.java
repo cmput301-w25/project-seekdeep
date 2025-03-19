f7351789d244295bad4f5162fef15b539b24cc9d
Kevin Tu <tukevin274@gmail.com>
2025-03-19 17:23:29 -0600

    Re-add missing comments

--- code/app/src/main/java/com/example/project_seekdeep/MoodHistoryFragment.java
+++ code/app/src/main/java/com/example/project_seekdeep/MoodHistoryFragment.java
@@ -214,11 +214,14 @@ public class MoodHistoryFragment extends Fragment {
         moodListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Bundle up the original Mood object that was clicked on
                 Bundle moodBundle = new Bundle();
                 moodBundle.putSerializable("mood", moodArrayList.get(position));
 
+                // This is used to navigate back and forth between a mood comment section and the feed or history
                 FragmentManager fragManager = getParentFragmentManager();
 
+                // Create new fragment and send Mood off into new fragment
                 ViewMoodDetailsFragment viewMoodDetailsFragment = new ViewMoodDetailsFragment();
                 viewMoodDetailsFragment.setArguments(moodBundle);
 
