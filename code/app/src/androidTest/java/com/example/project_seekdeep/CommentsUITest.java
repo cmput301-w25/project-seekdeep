3284788d1f95390842120cdf601a43a380e5a873
Kevin Tu <tukevin274@gmail.com>
2025-03-26 15:44:40 -0600

    Add UI testing for comments

---
+++ code/app/src/androidTest/java/com/example/project_seekdeep/CommentsUITest.java
@@ -0,0 +1,24 @@
+package com.example.project_seekdeep;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class CommentsUITest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    @Test
+    public void clickOnMoodShouldDisplayMoodComments() {
+        onView(withId())
+    }
+}
