add838719ecd689b1b0948772b3e6a193d53fbdc
Nancy Lin <nylin@ualberta.ca>
2025-03-29 00:06:53 -0600

    Add some non-functional unit tests

--- code/app/src/test/java/com/example/project_seekdeep/ImageProviderTest.java
+++ code/app/src/test/java/com/example/project_seekdeep/ImageProviderTest.java
@@ -1,22 +1,40 @@
 package com.example.project_seekdeep;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mock;
 
+import android.net.Uri;
+
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import java.util.Objects;
 
 
 // This testing file is taken from Jachelle Chan's MoodProvider code Seth's Lab-08 code
 // Taken by: NANCY lin
 // Taken on: March 26, 2025
+
+@PrepareForTest({Uri.class})
+@RunWith(PowerMockRunner.class)
 public class ImageProviderTest {
     @Mock
     private FirebaseFirestore mockFirestore;
@@ -24,11 +42,34 @@ public class ImageProviderTest {
     private CollectionReference mockMoodCollection;
     @Mock
     private DocumentReference mockDocReference;
+
+    @Mock
+    private FirebaseStorage mockStorage;
+    @Mock
+    private StorageReference mockImagesReference;
+    @Mock
+    private StorageReference mockSelectedImageReference;
+
+    @Mock
+    private Uri mockUploadUri;
+
+    @Mock
+    private Uri mockStorageUri;
+
+    @Mock
+    private UploadTask mockUploadTask;
+
+    private Uri imageUri;
+
     private MoodProvider moodProvider;
+
+    private ImageProvider imageProvider;
     private UserProfile testUser = new UserProfile("jshello", "tofu123");
 
+
+    //@PrepareForTest({Uri.class})
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         // Start up mocks
         MockitoAnnotations.openMocks(this);
         // Define the behaviour we want during our tests. This part is what avoids the calls to firestore.
@@ -36,34 +77,42 @@ public class ImageProviderTest {
         when(mockMoodCollection.document()).thenReturn(mockDocReference);
         when(mockMoodCollection.document(anyString())).thenReturn(mockDocReference);
 
+        when(mockStorage.getReference()).thenReturn(mockImagesReference);
+        when(mockStorage.getReference().child("Images/")).thenReturn(mockImagesReference);
+        //when(mockImagesReference.child( anyString() ).thenReturn(mockSelectedImageReference);
+
+        PowerMockito.mockStatic(Uri.class);
+        mockUploadUri = mock(Uri.class);
+        PowerMockito.when(Uri.class, "parse", anyString()).thenReturn(mockUploadUri);
+
+        //when(Objects.requireNonNull(mockUploadUri.getLastPathSegment())).thenReturn("123");
+        when(mockUploadUri.getLastPathSegment()).thenReturn("123");
+        when(mockImagesReference.child(any())).thenReturn(mockSelectedImageReference);
+        when(mockSelectedImageReference.putFile(any())).thenReturn(mockUploadTask);
+
+
+
         // make sure we have a fresh instance for each test
         MoodProvider.setInstanceForTesting(mockFirestore);
         moodProvider = MoodProvider.getInstance(mockFirestore);
+        imageProvider = ImageProvider.getInstance(mockStorage);
+
+        //imageUri = Uri.parse("123");
 
     }
 
+
+    //@PrepareForTest({Uri.class})
     @Test
-    public void testEditMood(){
+    public void testUploadImage(){
+
+        imageProvider.uploadImageToFirebase(mockUploadUri);
+        verify(mockSelectedImageReference).putFile(mockUploadUri);
+        //assertEquals(1, 0);
+
+
 
     }
 
-    @Test
-    public void testAddMood() {
-        Mood mood = new Mood(testUser, EmotionalStates.ANGER);
-        // add the mood
-        moodProvider.addMoodEvent(mood);
 
-        // verify that the add method did in fact add a mood to the collection
-        verify(mockDocReference).set(mood);
-    }
-
-    @Test
-    public void testDeleteMood() {
-        // create mood
-        Mood mood = new Mood(testUser, EmotionalStates.ANGER);
-        mood.setDocRef(mockDocReference);
-        // Call the delete mood and verify the firebase delete method was called.
-        moodProvider.deleteMood(mood);
-        verify(mockDocReference).delete();
-    }
 }
