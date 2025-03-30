683d60ea89b874ff6c29f3023678ea77a76248ba
Jshell0 <jachelle@ualberta.ca>
2025-03-29 22:29:50 -0600

    Implemented followingFragmentUI testing. Need to write more tests

---
+++ code/app/src/androidTest/java/com/example/project_seekdeep/FollowingFragmentUITest.java
@@ -0,0 +1,179 @@
+package com.example.project_seekdeep;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import static org.hamcrest.CoreMatchers.anything;
+
+import android.util.Log;
+
+import androidx.test.espresso.matcher.BoundedMatcher;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.auth.User;
+
+import org.hamcrest.Description;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Calendar;
+import java.util.Objects;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class FollowingFragmentUITest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario =
+            new ActivityScenarioRule<MainActivity>(MainActivity.class);
+
+    private final UserProfile testUser = new UserProfile("User1", "123");
+    private final UserProfile testUser2 = new UserProfile("User2", "123");
+    private final UserProfile testUser3 = new UserProfile("User3", "123");  // testUser3 will follow the other 2
+    private final FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private final CollectionReference usersRef = db.collection("UserDB");
+    private final CollectionReference moodsRef = db.collection("MoodDB");
+
+    @BeforeClass
+    public static void setup() {
+        // Specific address for emulated device to access our localHost
+        String androidLocalhost = "10.0.2.2";
+
+        int portNumber = 8080;
+        FirebaseFirestore.getInstance().useEmulator(androidLocalhost, portNumber);
+    }
+
+    @Before
+    public void seedDatabase() throws InterruptedException {
+        Calendar calendar = Calendar.getInstance();
+        Mood mood1 = new Mood(testUser, EmotionalStates.HAPPINESS, SocialSituations.ALONE, calendar.getTime(), "Done testing!");
+        calendar.add(Calendar.DAY_OF_YEAR, -1);
+        Mood mood2 = new Mood(testUser, EmotionalStates.CONFUSION, SocialSituations.WITH_ANOTHER, calendar.getTime(), "How to test?");
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -3);
+        Mood mood3 = new Mood(testUser, EmotionalStates.SADNESS, SocialSituations.CROWD, calendar.getTime(), "Midterms");
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -10);
+        Mood mood4 = new Mood(testUser, EmotionalStates.ANGER, SocialSituations.ALONE, calendar.getTime(), "Midterms are hard");
+
+        usersRef.document().set(testUser);
+
+        moodsRef.document().set(mood1);
+        moodsRef.document().set(mood2);
+        moodsRef.document().set(mood3);
+        moodsRef.document().set(mood4);
+
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -2);
+        mood1 = new Mood(testUser2, EmotionalStates.FEAR, SocialSituations.ALONE, calendar.getTime(), "Finals soon");
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -4);
+        mood2 = new Mood(testUser2, EmotionalStates.DISGUST, SocialSituations.ALONE, calendar.getTime(), "204, 229, 301 in one semester??");
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -5);
+        mood3 = new Mood(testUser2, EmotionalStates.SURPRISE, SocialSituations.CROWD, calendar.getTime(), "Didn't fail");
+        calendar = Calendar.getInstance();
+        calendar.add(Calendar.DAY_OF_YEAR, -6);
+        mood4 = new Mood(testUser2, EmotionalStates.SHAME, SocialSituations.SEVERAL_PEOPLE, calendar.getTime(), "Failed");
+
+        usersRef.document().set(testUser2);
+
+        moodsRef.document().set(mood1);
+        moodsRef.document().set(mood2);
+        moodsRef.document().set(mood3);
+        moodsRef.document().set(mood4);
+
+        // follower user1/testUser and user2/testUser2
+        testUser3.addFollowing(testUser.getUsername());
+        testUser3.addFollowing(testUser2.getUsername());
+        // the following list should look like [happiness, confusion, fear, sadness, disgust, surprise, shame, anger]
+        // log in first
+        scenario.getScenario().onActivity(activity -> activity.setCurrentUser(testUser3));
+        scenario.getScenario().onActivity(activity -> activity.successful_login());
+    }
+
+    @After
+    public void tearDown() {
+        String projectId = "project-seekdeep";
+        URL url = null;
+        try {
+            url = new URL("http://10.0.2.2:8080/emulator/v1/projects/" + projectId + "/databases/(default)/documents");
+        } catch (MalformedURLException exception) {
+            Log.e("URL Error", Objects.requireNonNull(exception.getMessage()));
+        }
+        HttpURLConnection urlConnection = null;
+        try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("DELETE");
+            int response = urlConnection.getResponseCode();
+            Log.i("Response Code", "Response Code: " + response);
+        } catch (IOException exception) {
+            Log.e("IO Error", Objects.requireNonNull(exception.getMessage()));
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+    }
+
+    @Test
+    public void testViewFollowingFragmentShowsAllMoodsOfFollowing() {
+        onView(withId(R.id.following_bottom_nav)).perform(click());
+        // the following list should look like [happiness, confusion, fear, sadness, disgust, surprise, shame, anger] in that order
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(0).onChildView(withId(R.id.emotion))
+                .check(matches(withText("😄 Happiness")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(1).onChildView(withId(R.id.emotion))
+                .check(matches(withText("🤔 Confusion")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(2).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.FEAR.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(3).onChildView(withId(R.id.emotion))
+                .check(matches(withText("☹️ Sadness")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(4).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.DISGUST.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(5).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.SURPRISE.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(6).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.SHAME.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(7).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.ANGER.toString())));
+    }
+
+    @Test
+    public void testLast3FilterShowsLast3FromEachFollowingUser() {
+        onView(withId(R.id.following_bottom_nav)).perform(click());
+        // the following list should look like
+        // [happiness(1), confusion(1), fear(2), sadness(1), disgust(2), surprise(2), shame(2), anger(1)] in that order initially
+        onView(withId(R.id.following_filter_button)).perform(click());
+        onView(withId(R.id.last_3_chip)).perform(click());
+        onView(withId(R.id.apply_filters_button)).perform(click());
+        // now the following list should look like [happiness(1), confusion(1), fear(2), sadness(1), disgust(2), surprise(2)]
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(0).onChildView(withId(R.id.emotion))
+                .check(matches(withText("😄 Happiness")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(1).onChildView(withId(R.id.emotion))
+                .check(matches(withText("🤔 Confusion")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(2).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.FEAR.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(3).onChildView(withId(R.id.emotion))
+                .check(matches(withText("☹️ Sadness")));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(4).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.DISGUST.toString())));
+        onData(anything()).inAdapterView(withId(R.id.listview_following)).atPosition(5).onChildView(withId(R.id.emotion))
+                .check(matches(withText(EmotionalStates.SURPRISE.toString())));
+    }
+
+}
