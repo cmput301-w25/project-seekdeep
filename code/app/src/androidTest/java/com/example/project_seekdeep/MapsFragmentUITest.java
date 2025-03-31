1b38f8b543fc17be26087df86bf6982df60d159d
Saurabh Singh Baghel <saurabh2864@gmail.com>
2025-03-31 14:36:13 -0600

    tests

--- code/app/src/androidTest/java/com/example/project_seekdeep/MapsFragmentUITest.java
+++ code/app/src/androidTest/java/com/example/project_seekdeep/MapsFragmentUITest.java
@@ -38,10 +38,9 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Objects;
 
+// Taken from MoodHistoryFragmentUiTest.java
 // https://developer.android.com/reference/androidx/test/uiautomator/UiDevice#getInstance(android.app.Instrumentation)
 // https://stackoverflow.com/questions/26449391/setelapsedrealtimenanos-call-in-android-mock-location-provider-app
 @RunWith(AndroidJUnit4.class)
@@ -74,24 +73,14 @@ public class MapsFragmentUITest {
         Thread.sleep(5000);
         Mood mood2 = new Mood(testUser, EmotionalStates.CONFUSION, new String[]{"How to test?", "With Another Person"});
         Thread.sleep(5000);
-        Mood mood3 = new Mood(testUser, EmotionalStates.SADNESS, new String[]{"Finals", "With a Crowd"});
-        // Mood with date outside of recent week
-        Thread.sleep(2000);
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, -10);
-        Mood mood4 = new Mood(testUser, EmotionalStates.ANGER, SocialSituations.ALONE, calendar.getTime(), "Let's get this done");
 
         usersRef.document().set(testUser);
 
         moodsRef.document().set(mood1);
         moodsRef.document().set(mood2);
-        moodsRef.document().set(mood3);
-        moodsRef.document().set(mood4);
 
         locationsRef.document().set(new UserLocation(53.5259933,-113.5234554, EmotionalStates.HAPPINESS,"sbaghel","mood1" ));
         locationsRef.document().set(new UserLocation(53.5259999,-113.5225457, EmotionalStates.CONFUSION, "sbaghel","mood2" ));
-        locationsRef.document().set(new UserLocation(53.5259343,-113.5245227, EmotionalStates.SADNESS, "sbaghel","mood3"));
-        locationsRef.document().set(new UserLocation(53.5253453,-113.5212687, EmotionalStates.ANGER,  "sbaghel","mood4"));
 
         UserProfile followedUser = new UserProfile("followedUser", "pass5678");
         usersRef.document().set(followedUser);
@@ -226,7 +215,7 @@ public class MapsFragmentUITest {
     }
 
     @Test
-    public void testFilter5KmRadiusFollowingCircleDisplay() throws InterruptedException {
+    public void testFilter5KmRadiusFollowingButtonClick() throws InterruptedException {
         // Go to maps fragment
         onView(withId(R.id.Map)).perform(click());
         Thread.sleep(2000);
@@ -267,25 +256,4 @@ public class MapsFragmentUITest {
         onView(withId(R.id.filter_mood_history)).check(matches(not(isEnabled())));
         onView(withId(R.id.map)).check(matches(isDisplayed()));
     }
-
-    //
-//    @Test
-//    public void testMoodHistoryDisplays(){
-//    }
-//
-//    @Test
-//    public void testMoodFollowingDisplays(){
-//
-//    }
-
-//    @Test
-//    public void testFilterMoodHistoryButtonClick() {
-//
-//    }
-//
-//    @Test
-//    public void testFilterMoodFollowingButtonClick() {
-//    }
-
-
 }
--- code/app/src/test/java/com/example/project_seekdeep/MapsFragmentTest.java
+++ code/app/src/test/java/com/example/project_seekdeep/MapsFragmentTest.java
@@ -1,8 +1,12 @@
 package com.example.project_seekdeep;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.junit.Before;
 import org.mockito.Mock;
@@ -14,11 +18,23 @@ public class MapsFragmentTest {
     @Mock
     private CollectionReference mockFollowingsCollection;
     @Mock
+    private CollectionReference mockLocationsCollection;
+    @Mock
     private DocumentReference mockDocRef;
+    @Mock
+    private GoogleMap mockGoogleMap;
+    @Mock
+    private Query mockQuery;
+    @Mock
+    private QuerySnapshot mockQuerySnapshot;
+    @Mock
+    private QueryDocumentSnapshot mockDocSnapshot;
+    @Mock
+    private MainActivity mockActivity;
+    private MapsFragment mapsFragment;
 
     @Before
     public void setUp() {
-        // Start up mocks
-        MockitoAnnotations.openMocks(this);
     }
+
 }
